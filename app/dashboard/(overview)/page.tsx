da36cbdf85c4ecf879be378599b82fd223f6fca0
walther <waltheroliveirapires@gmail.com>
2024-05-18 02:03:24 -0300

    nova tentativa correcao Revenue

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -22,7 +22,6 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          {' '}
           <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
